0900177b2ecc4a01c288c063af25dadd26071811
Nachum Yosef <nachumy7@gmail.com>
2022-09-07 11:32:56 +0300

    modify mui v5 with next js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,51 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 import Layout from '../components/Layout/Layout'
 import { useRouter } from 'next/router'
 import Home from './'
 import BoardProvider from '../store/board'
-import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
+import * as React from 'react'
+import type { AppProps } from 'next/app'
+import { CacheProvider, EmotionCache } from '@emotion/react'
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material'
+
+// import '@fontsource/roboto/300.css'
+// import '@fontsource/roboto/400.css'
+// import '@fontsource/roboto/500.css'
+// import '@fontsource/roboto/700.css'
+
+import createEmotionCache from '../util/createEmotionCache'
+import lightThemeOptions from '../util/theme'
+import '../styles/globals.css'
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
+
+const clientSideEmotionCache = createEmotionCache()
+
+const lightTheme = createTheme(lightThemeOptions)
+
+const MyApp: React.FunctionComponent<MyAppProps> = (props) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
   const router = useRouter()
-
   const homePagePath = router.pathname === '/'
 
-  if (homePagePath) return (
-    < Home />
-  )
-
   return (
-    <BoardProvider>
-      <Head>
-        <title>TaskNDrop</title>
-      </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </BoardProvider>
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={lightTheme}>
+        <BoardProvider>
+          <CssBaseline />
+          {homePagePath && <Home />}
+          {!homePagePath && (
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          )}
+        </BoardProvider>
+      </ThemeProvider>
+    </CacheProvider>
   )
 }
 
+export default MyApp;
 
-
-export default MyApp
