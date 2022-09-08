e968ddca041cc3267fc76424fffdda6d4038d359
Nachum Yosef <nachumy7@gmail.com>
2022-09-08 15:18:45 +0300

    add Editable mode for BoardHeader

--- components/boards/board-header/boardHeaderTop.tsx
+++ components/boards/board-header/boardHeaderTop.tsx
@@ -1,13 +1,38 @@
+import { useEffect, useState, useContext } from 'react'
+import InlineEdit from '../../UI/inline-edit/InlineEdit'
 import classes from './BoardAppHeader.module.scss'
-
+import { BoardContext } from '../../../store/board'
 
 const BoardHeaderTop = () => {
+
+
+    const { board } = useContext(BoardContext)
+
+    const [value, setValue] = useState(board!.title)
+    const [editingMode, setEditingMode] = useState(false)
+
+    const startEditingHandler = () => {
+        setEditingMode(() => true)
+    }
+
+    useEffect(() => {
+        if (value && value !== board?.title && value.trim().length !== 0) {
+            console.log(value)
+        }
+    }, [editingMode])
+
     return (
         <div className={classes['board-header-top']}>
             <div className={classes['board-header-left']}>
                 <div className={classes['board-name']}>
-                    <div className={classes['board-title-component']}>
-                        <p>test</p>
+                    <div onClick={startEditingHandler} className={editingMode ? classes['board-title-component-active'] : classes['board-title-component']}>
+                        {!editingMode &&
+                            <span
+                                className={classes['text-content']}
+                            >
+                                {value}
+                            </span>}
+                        {editingMode && < InlineEdit value={value} setValue={setValue} editMode={setEditingMode} />}
                     </div>
                     <div className={classes['board-header-show-description']}>
                         <span className={classes['board-header-icon-star']} ></span>
