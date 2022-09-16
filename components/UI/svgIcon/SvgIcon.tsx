04613e4b06035864d9109be15dbb2cac270414ab
Nachum Yosef <nachumy7@gmail.com>
2022-09-16 10:16:26 +0300

    change SvgIcon componnents to new distaracthring and short circiut

--- components/UI/svgIcon/SvgIcon.tsx
+++ components/UI/svgIcon/SvgIcon.tsx
@@ -1,14 +1,14 @@
 
-const SvgIcon: React.FC<{ path: string, fill?: string, classN?: string, width?: string, height?: string, viewBox?: string }> = (props) => {
+const SvgIcon: React.FC<{ path: string, fill?: string, classN?: string, width?: string, height?: string, viewBox?: string }> = ({ viewBox, fill, width, height, classN, path }) => {
     return (
         <svg
-            viewBox={props.viewBox ? props.viewBox : '0 0 20 20'}
-            fill={props.fill ? props.fill : 'currentColor'}
-            width={props.width ? props.width : '24'}
-            height={props.height ? props.height : '24'}
+            viewBox={viewBox || '0 0 20 20'}
+            fill={fill || 'currentColor'}
+            width={width || '24'}
+            height={height || '24'}
             aria-hidden="true"
-            className={`icon_component ${props.classN ? props.classN : ''}`}
-            dangerouslySetInnerHTML={{ __html: props.path }} >
+            className={`icon_component ${classN || ''}`}
+            dangerouslySetInnerHTML={{ __html: path }} >
         </svg>
     )
 }
