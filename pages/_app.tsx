ed32b2302071f1b2cf03a87e472a5b36fc7ce0fe
Nachum Yosef <nachumy7@gmail.com>
2022-09-21 23:47:00 +0300

    add gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ import * as React from 'react'
 import type { AppProps } from 'next/app'
 import { CacheProvider, EmotionCache } from '@emotion/react'
 import { ThemeProvider, CssBaseline, createTheme } from '@mui/material'
+import * as gtag from '../lib/gtag'
 
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
@@ -16,6 +17,8 @@ import '@fontsource/roboto/700.css'
 
 import createEmotionCache from '../util/createEmotionCache'
 import lightThemeOptions from '../util/theme'
+import Script from 'next/script'
+import Gtag from '../components/script/Gtag'
 interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache
 }
@@ -29,8 +32,24 @@ const MyApp: React.FunctionComponent<MyAppProps> = (props) => {
   const router = useRouter()
   const homePagePath = router.pathname === '/'
 
+  React.useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('hashChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('hashChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+
+
+
   return (
     <CacheProvider value={emotionCache}>
+      <Gtag />
       <ThemeProvider theme={lightTheme}>
         <BoardProvider>
           <CssBaseline />
