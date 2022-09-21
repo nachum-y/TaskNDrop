f10d4cfe67034469814446cd51669d9938e7bb17
Nachum Yosef <nachumy7@gmail.com>
2022-09-21 11:03:38 +0300

    add Link To homePage

--- components/app-navigation/AppNavigation.tsx
+++ components/app-navigation/AppNavigation.tsx
@@ -5,12 +5,15 @@ import classes from './AppNavigation.module.scss'
 import SurfaceIconWrapper from './SurfaceIconWrapper'
 import logoIcon from '../../assets/logos/logo_icon.png'
 import { surfaceIcon } from '../../service/svgIcon'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 
 const AppNavigation = () => {
 
     const [selectedTube, setSelectedTube] = useState('work management')
 
+    const router = useRouter()
     const selectedTabHandler = (tabName: string) => {
         setSelectedTube(tabName)
     }
@@ -19,15 +22,19 @@ const AppNavigation = () => {
         <nav className={classes['surface-control']} aria-label="Views and Tools">
             <div className={classes['surface-control-component']}>
 
-
-                <SurfaceIconWrapper image={logoIcon}
-                    selected={selectedTube === 'Home Page' ? true : false}
-                    width='40'
-                    height='40'
-                    onSelectTab={selectedTabHandler}
-                    name='Home Page'
-                />
-
+                <Link href={{
+                    pathname: '/',
+                }}>
+                    <div className={classes['home-page-link']}>
+                        <SurfaceIconWrapper image={logoIcon}
+                            selected={selectedTube === 'Home Page' ? true : false}
+                            width='40'
+                            height='40'
+                            onSelectTab={selectedTabHandler}
+                            name='Home Page'
+                        />
+                    </div>
+                </Link>
                 <div className={classes['surface-divider']}></div>
 
                 <div className={classes['top-navigation-items-area']}>
